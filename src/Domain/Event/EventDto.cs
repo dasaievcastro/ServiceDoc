ead16cc46949fda6bb349074303371c6dfe2d033
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2019-04-05 21:14:54 +0100

    * ServiceDoc.sln: Create Web Interface to manage data
    * Program.cs:
    * Startup.cs:
    * Web.csproj:
    * appsettings.json:
    * EventDto.cs:
    * List.cshtml:
    * appsettings.Development.json:
    * NewOrEdit.cshtml:
    * EventController.cs:

---
+++ src/Domain/Event/EventDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Domain.Event
+{
+    public class EventDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+    }
+}
---
+++ src/Web/Controllers/EventController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("List");
+        }
+
+        public IActionResult New()
+        {
+            return View("NewOrEdit");
+        }
+    }
+}
